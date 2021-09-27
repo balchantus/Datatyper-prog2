8f9c07f265535a0fb753a066b5dc6a5434161f1c
balchantus <89977635+balchantus@users.noreply.github.com>
2021-09-27 10:03:19 +0200

    Add files via upload

---
+++ Datatyper/Datatyp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datatyper
+{
+    class Datatyp
+    {
+        public static string[] typ = { "integer", "float", "bool", "char", "string" };
+        public static string svar = " ";
+        public static string input = " ";
+
+        public string Data()
+        {
+            input = Console.ReadLine();
+
+            if (Int32.TryParse(input, out _))
+            {
+                svar = typ[0];
+                return svar;
+            }
+
+            else if (float.TryParse(input, out _))
+            {
+                svar = typ[1];
+                return svar;
+            }
+
+            else if (bool.TryParse(input, out _))
+            {
+                svar = typ[2];
+                return svar;
+            }
+
+            else if (char.TryParse(input, out _))
+            {
+                svar = typ[3];
+                return svar;
+            }
+
+            else if (input == " ")
+            {
+                svar = "Vänligen mata in något";
+                return svar;
+            }
+
+            else
+            {
+                svar = typ[4];
+                return svar;
+            }
+        }
+    }
+}
---
+++ Datatyper/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Datatyper.Datatyp;
+
+namespace Datatyper
+{
+    class Datatyper
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Detta är ett program som kollar vilken datatyp som matas in!");
+
+            while (true)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Skriv Q för att avsluta! ");
+                Console.Write("Mata in något för att ta reda på dess datatyp: ");
+
+                if (input == "Q" || input == "q")
+                {
+                    break;
+                }
+
+                Datatyp Dtyp = new Datatyp();
+                Dtyp.Data();
+
+                Console.WriteLine($"'{input}'Detta är datatypen " + svar);
+            }
+        }
+    }
+}

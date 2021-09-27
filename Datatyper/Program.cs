a3c5f0b55550747897a313ddfe4de80182a22888
balchantus <89977635+balchantus@users.noreply.github.com>
2021-09-27 10:53:35 +0200

    Add files via upload

---
+++ Datatyper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static Datatyper.Datatyp;
+
+namespace Datatyper
+{
+    class Datatyper
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Detta är ett program som kollar vilken datatyp som matas in!");
+
+            Datatyp Dtyp = new Datatyp();
+
+            while (true)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Skriv Q för att avsluta! ");
+                Console.Write("Mata in något för att ta reda på dess datatyp: ");
+
+                if (input == "Q" || input == "q")
+                {
+                    break;
+                }
+
+                Dtyp.Data();
+
+                Console.WriteLine($"'{input}'Detta är datatypen " + svar);
+            }
+        }
+    }
+}
